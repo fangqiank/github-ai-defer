1988c00329f788eefc985dad94c5269d6e0f2fc2
fangqiank <fangqiank@yahoo.com>
2023-03-09 10:10:33 +0800

    first commit

---
+++ app/api/githubProfile/[usernameOrExecId]/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import {getGithubInfo, generateGithubProfile} from '@/defer/generateGitHubProfile'
+
+export async function POST(
+  _request: NextRequest,
+  { params }: { params: { usernameOrExecId: string } }
+) {
+  const res = await generateGithubProfile(params.usernameOrExecId)
+  return NextResponse.json(res);
+}
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { usernameOrExecId: string } }
+) {
+  return NextResponse.json({});
+}
